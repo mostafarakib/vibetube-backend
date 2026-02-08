e1a22af97283387772a2160b16dc0b2f7de41c1c
mostafarakib <mostafa.rakib.10@gmail.com>
2026-02-08 23:18:19 +0600

    added file handling and completed user registration method

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,8 @@
 import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { uploadToCloudinary } from "../utils/cloudinary.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   const { username, email, password, fullName } = req.body;
@@ -55,6 +57,41 @@ const registerUser = asyncHandler(async (req, res) => {
       "User with the same email or username already exists"
     );
   }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar is required");
+  }
+
+  const avatar = await uploadToCloudinary(avatarLocalPath);
+  const coverImage = await uploadToCloudinary(coverImageLocalPath);
+
+  if (!avatar) {
+    throw new ApiError(400, "Failed to upload avatar image. Please try again.");
+  }
+
+  const user = await User.create({
+    username: username.trim().toLowerCase(),
+    fullName,
+    avatar: avatar.secure_url,
+    coverImage: coverImage?.secure_url || "",
+    email,
+    password,
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "Failed to create user. Please try again.");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, "User registered successfully", createdUser));
 });
 
 export { registerUser };
